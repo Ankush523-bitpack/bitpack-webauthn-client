c71f2392c03d730aaf3d3c7aefd5b602ae246221
Ankush523-bitpack <ankush.dutta@bitpack.me>
2023-09-21 11:46:04 +0530

    client-demo for passkeys

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,155 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { client } from '@passwordless-id/webauthn';
 
 export default function Home() {
+  const [username, setUsername] = useState('');
+  const [message, setMessage] = useState('');
+  const [walletAddress, setWalletAddress] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);  // New state for logged-in status
+
+  useEffect(() => {
+    // On initial render, check localStorage for logged-in status and wallet address
+    const savedLoggedInStatus = localStorage.getItem('isLoggedIn');
+    const savedWalletAddress = localStorage.getItem('walletAddress');
+    
+    if (savedLoggedInStatus) {
+      setIsLoggedIn(true);
+    }
+    if (savedWalletAddress) {
+      setWalletAddress(savedWalletAddress);
+    }
+  }, []);
+
+  const register = async () => {
+    try {
+      const challengeResponse = await axios.post('http://localhost:3000/request-challenge', { username });
+      const challenge = challengeResponse.data.challenge;
+
+      const registration = await client.register(username, challenge, {
+        authenticatorType: "auto",
+        userVerification: "required",
+        timeout: 60000,
+        attestation: false,
+        debug: false,
+      });
+
+    console.log(registration);
+
+      await axios.post('http://localhost:3000/register', registration);
+
+      setMessage('Registration successful!');
+    } catch (error:any) {
+      setMessage('Registration failed: ' + error.message);
+    }
+  };
+
+  const getWalletAddress = async () => {
+    try {
+      const response = await axios.get(`http://localhost:3000/credentials/${username}`);
+      if (response.data && response.data.walletAddress) {
+        setWalletAddress(response.data.walletAddress);
+        localStorage.setItem('walletAddress', response.data.walletAddress);  // Save to localStorage
+      } else {
+        throw new Error("Wallet address not found");
+      }
+    } catch (error:any) {
+      setMessage('Failed to retrieve wallet address: ' + error.message);
+    }
+  };
+
+  const authenticate = async () => {
+    try {
+        const challengeResponse = await axios.post('http://localhost:3000/request-challenge', { username });
+        const challenge = challengeResponse.data.challenge;
+        console.log("challenge for verification : ",challenge);
+        // Fetch the credential IDs for the user
+        const credentialsResponse = await axios.get(`http://localhost:3000/credentials/${username}`);
+        const credentials = credentialsResponse.data.credentialIds;
+
+        const authentication = await client.authenticate(
+            credentials,
+            challenge,
+            {
+                authenticatorType: "auto",
+                userVerification: "required",
+                timeout: 60000,
+            }
+        );
+
+        await axios.post('http://localhost:3000/authenticate', { challenge, authentication });
+
+        setMessage('Authentication successful!');
+        setIsLoggedIn(true);  // Set logged-in status to true
+        localStorage.setItem('isLoggedIn', 'true');  // Save to localStorage
+        await getWalletAddress();
+    } catch (error:any) {
+        setMessage('Authentication failed: ' + error.message);
+    }
+  };
+
+  const signOut = () => {
+    setIsLoggedIn(false);
+    setWalletAddress('');
+    setUsername('');
+    setMessage('');
+    localStorage.removeItem('isLoggedIn');
+    localStorage.removeItem('walletAddress');
+  };
+
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 text-black">
+      <div className="p-8 bg-white rounded-xl shadow-md w-[500px] space-y-4">
+        { !isLoggedIn ? (
+          <div className="space-y-4">
+            <div>
+              <label htmlFor="username" className="block text-sm font-medium text-gray-600">Username</label>
+              <input
+                id="username"
+                type="text"
+                value={username}
+                onChange={e => setUsername(e.target.value)}
+                placeholder="Username"
+                className="mt-1 p-2 w-full border rounded-md"
+              />
+            </div>
+            <div className="flex justify-between space-x-4">
+              <button 
+                onClick={register}
+                className="w-full p-2 text-white bg-blue-500 rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+              >
+                Register
+              </button>
+              <button 
+                onClick={authenticate}
+                className="w-full p-2 text-white bg-green-500 rounded hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50"
+              >
+                Login
+              </button>
+            </div>
+          </div>
+        ) : (
+          <div className="space-y-4">
+            <div className="flex justify-end">
+              <button 
+                onClick={signOut}
+                className="p-2 text-white bg-red-500 rounded hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50"
+              >
+                Sign Out
+              </button>
+            </div>
+            {walletAddress && (
+              <div>
+                <label className="block text-sm font-medium text-gray-600">Wallet Address:</label>
+                <p className="mt-1 text-gray-900">{walletAddress}</p>
+              </div>
+            )}
+          </div>
+        )}
+        {message && <p className="text-center text-red-500">{message}</p>}
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
+  
 }
