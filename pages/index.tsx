65c39939b67f5b9cf8d17a6df48d071c1cbca107
Ankush523-bitpack <ankush.dutta@bitpack.me>
2023-10-08 12:34:05 +0530

    add: sdk implementation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,29 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { useRouter } from 'next/router'; 
-import { client } from '@passwordless-id/webauthn';
-import Cookie from 'js-cookie';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/router";
+import { client } from "@passwordless-id/webauthn";
+import Cookie from "js-cookie";
+import { authenticateUser, registerUser } from "uim-sdk-ts";
 
 export default function Home() {
-  const [username, setUsername] = useState('');
-  const [message, setMessage] = useState('');
+  const [username, setUsername] = useState("");
+  const [message, setMessage] = useState("");
   const router = useRouter();
 
   useEffect(() => {
     // Check if user is already logged in
-    const loggedInUsername = Cookie.get('username');
-    const walletAddress = Cookie.get('walletAddress');
+    const loggedInUsername = Cookie.get("username");
+    const walletAddress = Cookie.get("walletAddress");
     if (loggedInUsername && walletAddress) {
       router.push(`/dashboard`);
     }
   }, [router]);
 
+  const origin = "http://localhost:3001";
+
   const register = async () => {
     try {
-      const challengeResponse = await axios.post('https://uim-alpha.meroku.org/request-challenge', { username });
+      const challengeResponse = await axios.post('http://localhost:3000/request-challenge', { username });
       const challenge = challengeResponse.data.challenge;
 
       const registration = await client.register(username, challenge, {
@@ -31,11 +34,19 @@ export default function Home() {
         debug: false,
       });
 
-      await axios.post('https://uim-alpha.meroku.org/register', registration);
+      await axios.post('http://localhost:3000/register', registration);
       setMessage('Registration successful!');
     } catch (error: any) {
       setMessage('Registration failed: ' + error.message);
     }
+    // try {
+    //   const res = await registerUser(username,origin);
+    //   setMessage("Registration successful!");
+    //   console.log(res)
+    // } 
+    // catch (error: any) {
+    //   setMessage(error.message);
+    // }
   };
 
   const authenticate = async () => {
@@ -55,7 +66,7 @@ export default function Home() {
       await axios.post('https://uim-alpha.meroku.org/authenticate', { challenge, authentication });
 
       setMessage('Authentication successful!');
-      
+
       const response = await axios.get(`https://uim-alpha.meroku.org/credentials/${username}`);
       if (response.data && response.data.walletAddress) {
           Cookie.set('username', username);
@@ -68,6 +79,16 @@ export default function Home() {
     } catch (error: any) {
       setMessage('Authentication failed: ' + error.message);
     }
+    // try {
+    //   const res = await authenticateUser(username,origin);
+    //   console.log("result Auth : ", res)
+    //   setMessage("Authentication successful!");
+    //   Cookie.set("username", username);
+    //   Cookie.set("walletAddress", res);
+    //   router.push("/dashboard"); // Removed the query parameters here
+    // } catch (error: any) {
+    //   setMessage("Authentication failed: " + error.message);
+    // }
   };
 
   return (
@@ -75,24 +96,29 @@ export default function Home() {
       <div className="p-8 bg-white rounded-xl shadow-md w-[500px] space-y-4">
         <div className="space-y-4">
           <div>
-            <label htmlFor="username" className="block text-sm font-medium text-gray-600">Username</label>
+            <label
+              htmlFor="username"
+              className="block text-sm font-medium text-gray-600"
+            >
+              Username
+            </label>
             <input
               id="username"
               type="text"
               value={username}
-              onChange={e => setUsername(e.target.value)}
+              onChange={(e) => setUsername(e.target.value)}
               placeholder="Username"
               className="mt-1 p-2 w-full border rounded-md"
             />
           </div>
           <div className="flex justify-between space-x-4">
-            <button 
+            <button
               onClick={register}
               className="w-full p-2 text-white bg-blue-500 rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
             >
               Register
             </button>
-            <button 
+            <button
               onClick={authenticate}
               className="w-full p-2 text-white bg-green-500 rounded hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50"
             >
